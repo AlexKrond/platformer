4ed804cf2f8fcfa7f8adc753c759ffe18d0efa6e
Alex Kashko <alexkrond@yahoo.com>
2019-08-14 18:04:50 +0300

    Добавлено движение с ускорением

--- v2.0/js/main.js
+++ v2.0/js/main.js
@@ -6,10 +6,8 @@ const context = canvas.getContext("2d");
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
 
-setInterval(update, 1000/60);
-
-
-const hw = 32,
+const platformsNum = 20,
+      hw = 32,
       hh = hw;
 let hx = canvas.width / 2 ^ 0,
     hy = canvas.height - hh,
@@ -19,17 +17,56 @@ let hx = canvas.width / 2 ^ 0,
     goRight = false,
     platforms = [];
 
-update();
+
+init();
+setInterval(update, 1000/60);
+
+
+function init() {
+
+  for (let i = 0; i < platformsNum; i++) {
+    platforms[i] = {
+      x: Math.random()*canvas.width - 100,
+      y: Math.random()*canvas.height,
+      w: Math.random()*150 + 100,
+      h: Math.random()*15 + 15
+    };
+  }
+
+}
 
 function update() {
 
-  hxv = goLeft ? -2 : goRight ? 2 : 0;
+  // Движение без ускорения
+  // hxv = goLeft ? -2 : goRight ? 2 : 0;
+
+  // Движение с ускорением
+  switch (true) {
+    case goLeft:
+      hxv = hxv ?  (hxv * 1.01) : -2;
+      break;
+    case goRight:
+      hxv = hxv ? (hxv * 1.01) : 2;
+      break;
+    default:
+      hxv = 0;
+  }
+
+  //
+  // Движение с ускорение и замедлением будет
+  //
 
   hx += hxv;
   hy += hyv;
 
   context.fillStyle = "black";
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "white";
+  for (let i = 0; i < platformsNum; i++) {
+    context.fillRect(platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h);
+  }
+
   context.fillStyle = "red";
   context.fillRect(hx, hy, hw, hh);
 }
@@ -43,6 +80,7 @@ function keyDown(event) {
     case "ArrowRight":
       goRight = true;
       break;
+    // case "ArrowUp":
   }
 }
 
