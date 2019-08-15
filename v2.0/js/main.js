85160071c9dfa1840887238e9984375c7b0777df
Alex Kashko <alexkrond@yahoo.com>
2019-08-15 14:12:09 +0300

    Добавлены коллизии, плавная остановка, поправлен отскок, добавлен экран WIN

--- v2.0/js/main.js
+++ v2.0/js/main.js
@@ -7,7 +7,11 @@ window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
 
 const platformsNum = 20,
-      jumpForce = -8,
+      acceleration = 0.1,
+      maxSpeed = 7,
+      startSpeed = 2,
+      jumpForce = 8,
+      bound = 0.78,
       gravity = 0.3,
       hw = 32,
       hh = hw;
@@ -17,77 +21,135 @@ let hx = canvas.width / 2 ^ 0,
     hyv = 0,
     goLeft = false,
     goRight = false,
-    platforms = [];
+    jump = false,
+    platforms = [
+      {
+        x: -2000,
+        y: canvas.height,
+        w: canvas.width + 4000,
+        h: 50
+      }
+    ];
 
 
 init();
-setInterval(update, 1000/60);
+const gameLoop = setInterval(update, 1000/50);
 
 
 function init() {
 
   for (let i = 0; i < platformsNum; i++) {
-    platforms[i] = {
+    platforms.push({
       x: Math.random()*canvas.width - 100,
       y: Math.random()*canvas.height,
       w: Math.random()*150 + 100,
       h: Math.random()*15 + 15
-    };
+    })
   }
 
 }
 
 function update() {
 
-  // Движение без ускорения
-  // hxv = goLeft ? -2 : goRight ? 2 : 0;
-
-  // Движение с ускорением
-  switch (true) {
-    case goLeft:
-      hxv = hxv ?  (hxv * 1.01) : -2;
-      break;
-    case goRight:
-      hxv = hxv ? (hxv * 1.01) : 2;
-      break;
-    default:
-      hxv = 0;
-  }
-
-  //
-  // Движение с ускорение и замедлением будет
-  //
-
-  hyv = hyv ? (hyv + gravity) : 0;
-
-
-  hx += hxv;
-  hy += hyv;
-
-  if (hy >= (canvas.height - hh)) {
-    if (hyv && hyv > 1) {
-      hyv = -hyv + 2;
-    } else {
-      hy = canvas.height - hh;
-      hyv = 0;
-    }
-  }
-
-
+  // Отрисовка игровых элементов
   context.fillStyle = "black";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   context.fillStyle = "white";
-  for (let i = 0; i < platformsNum; i++) {
+  for (let i = 0; i < platforms.length; i++) {
     context.fillRect(platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h);
   }
 
   context.fillStyle = "red";
   context.fillRect(hx, hy, hw, hh);
+
+
+  // WIN!!!
+  // if (hy <= 0) {
+  //   win();
+  // }
+
+
+  // Движение с ускорением и остановкой
+  switch (true) {
+    case goLeft:
+      if (hxv <= 0) {
+        hxv = hxv ? (hxv < -maxSpeed) ? hxv : (hxv - acceleration) : -startSpeed;
+      } else {
+        hxv = (hxv < 0.5 && hxv > -0.5) ? 0 : hxv + (-hxv * 0.1);
+      }
+      break;
+    case goRight:
+      if (hxv >= 0) {
+        hxv = hxv ? (hxv > maxSpeed) ? hxv : (hxv + acceleration) : startSpeed;
+      } else {
+        hxv = (hxv < 0.5 && hxv > -0.5) ? 0 : hxv + (-hxv * 0.1);
+      }
+      break;
+    default:
+      hxv = (hxv < 0.5 && hxv > -0.5) ? 0 : hxv + (-hxv * 0.1);
+  }
+
+
+  // Ускорение при падении
+  hyv = hyv ? (hyv + gravity) : gravity;
+
+
+  // Проверка коллизий
+  let collide = false;
+  for (let i = 0; i < platforms.length; i++) {
+    if (isCollide(hx + hxv, hy + hyv, hw, hh, platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h)) {
+      hy = platforms[i].y - hh;
+      collide = true;
+      rebound(platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h);
+    }
+  }
+
+  hx += hxv;
+  hy = collide ? hy : (hy + hyv);
 }
 
-// TODO: Исправить баг при одновременном нажатии клавиш (вверх + право/лево + лево/право [право/лево отпускаем])
 
+// Если выиграл
+function win() {
+  clearInterval(gameLoop);
+
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "white";
+  context.font = "100px Arial";
+  context.fillText("WIN!", canvas.width / 2 - 100, canvas.height / 2);
+}
+
+
+// Проверка коллизии
+function isCollide(hx, hy, hw, hh, px, py, pw, ph) {
+  return !((hx + hw) < px ||
+          hy > (py + ph) ||
+          hx > (px + pw) ||
+          (hy + hh) < py);
+}
+
+
+// Вертикальный отскок от поверхностей
+function rebound(px, py, pw, ph) {
+  if ((hy + hh) >= py) {
+    if (jump) {
+      hyv = -jumpForce;
+
+    } else if (hyv && ((hyv - (hyv * bound)) > 1)) {
+      hyv = -hyv * bound;
+
+    } else {
+      hy = py - hh;
+      hyv = 0;
+    }
+  }
+}
+
+
+// Нажатие клавиши
 function keyDown(event) {
   switch (event.code) {
     case "ArrowLeft":
@@ -99,11 +161,13 @@ function keyDown(event) {
       goLeft = false;
       break;
   }
-  if (event.code === "ArrowUp" && hyv === 0) {
-    hyv = jumpForce;
+  if (event.code === "ArrowUp") {
+    jump = true;
   }
 }
 
+
+// Отпускание клавиши
 function keyUp(event) {
   switch (event.code) {
     case "ArrowLeft":
@@ -112,6 +176,8 @@ function keyUp(event) {
     case "ArrowRight":
       goRight = false;
       break;
+    case "ArrowUp":
+      jump = false;
+      break;
   }
 }
-
