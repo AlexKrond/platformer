d1318a04d0e8f0790d345583d8476e55be357ebb
Alex Kashko <alexkrond@yahoo.com>
2019-08-20 11:40:34 +0300

    Добавлено в спрайт выбор правильной стороны и обработка прыжка

--- v3.0/js/Sprite.js
+++ v3.0/js/Sprite.js
@@ -8,7 +8,6 @@ class Sprite {
     this.frameWidth = props.frameWidth;
     this.frameHeight = props.frameHeight;
 
-    this.currentState = null;
     this.currentIndex = 0;
 
     this.states = {
@@ -19,27 +18,52 @@ class Sprite {
       standLeft: [5],
       moveLeft: [5, 6, 7, 8]
     };
+
+    this.currentState = this.states.standRight;
   }
 
   update(deltaTime) {
     if (this.gameObject.xv < 0) {
       this.currentState = this.states.moveLeft;
       this.currentIndex = (this.currentIndex >= this.currentState[0] &&
-                           this.currentIndex <= this.currentState[this.currentState.length - 1] + 1) ?
-                             this.currentIndex:
-                             this.currentState[0];
+          this.currentIndex < this.currentState[this.currentState.length - 1] + 1) ?
+          this.currentIndex :
+          this.currentState[0];
     }
 
     if (this.gameObject.xv > 0) {
       this.currentState = this.states.moveRight;
       this.currentIndex = (this.currentIndex >= this.currentState[0] &&
-                           this.currentIndex <= this.currentState[this.currentState.length - 1] + 1) ?
-                             this.currentIndex:
-                             this.currentState[0];
+          this.currentIndex < this.currentState[this.currentState.length - 1] + 1) ?
+          this.currentIndex :
+          this.currentState[0];
     }
 
     if (this.gameObject.xv === 0) {
-      this.currentState = this.states.standLeft;
+      if (this.currentState === this.states.moveLeft ||
+          this.currentState === this.states.jumpLeft ||
+          this.currentState === this.states.standLeft) {
+        this.currentState = this.states.standLeft;
+      }
+
+      if (this.currentState === this.states.moveRight ||
+          this.currentState === this.states.jumpRight ||
+          this.currentState === this.states.standRight) {
+        this.currentState = this.states.standRight;
+      }
+
+      this.currentIndex = this.currentState[0];
+    }
+
+    if (this.gameObject.yv > 10) {
+      if (this.currentState === this.states.moveLeft || this.currentState === this.states.standLeft) {
+        this.currentState = this.states.jumpLeft;
+      }
+
+      if (this.currentState === this.states.moveRight || this.currentState === this.states.standRight) {
+        this.currentState = this.states.jumpRight;
+      }
+
       this.currentIndex = this.currentState[0];
     }
 
