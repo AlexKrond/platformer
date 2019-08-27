ee20b41d109a7bc9c23f46dbcd3d84657c3bbe95
Alex Kashko <alexkrond@yahoo.com>
2019-08-27 16:37:30 +0300

    ISSUE-46 Добавлен выбор платформы с правой или левой стороны, и прыжок на неё

--- v3.0/js/EnemyAI.js
+++ v3.0/js/EnemyAI.js
@@ -5,88 +5,131 @@ class EnemyAI {
     this.enemy = enemy;
     this.game = game;
 
-    this.maxJumpHeight = (this.enemy.jumpForce * this.enemy.jumpForce * 0.9) / (2 * this.game.gravity);
+    this.maxJumpHeight = (this.enemy.jumpForce * this.enemy.jumpForce * 0.99) / (2 * this.game.gravity);
     this.targetPlatform = null;
+    this.isJumping = false;
   }
 
   update() {
+    if (!this.enemy.lastBottomCollidePlatform) return;
+
+    // Перекрасить все платформы обратно в черный
+    this.game.platforms.forEach(p => p.color = "black");
+
     if (this.targetPlatform) {
-      // если на подлете goleft, Right = false, как только this.y < p.y => продолжить движение;
-    }
+      // Выделим её красным
+      this.targetPlatform.color = "red";
 
-    if ((this.enemy.x + this.enemy.w) < (this.game.hero.x - this.game.hero.w)) {
-      if (this.enemy.lastBottomCollidePlatform && (this.enemy.x + this.enemy.w) < (this.enemy.lastBottomCollidePlatform.x + this.enemy.lastBottomCollidePlatform.w)) {
-        this.enemy.goLeft = false;
-        this.enemy.goRight = true;
-      } else {
-        this.enemy.goLeft = false;
-        this.enemy.goRight = false;
+      if (this.enemy.lastBottomCollidePlatform === this.targetPlatform) {
+        this.targetPlatform = null;
+        this.isJumping = false;
+        return;
+      }
 
-        const platform = this.getRightPlat();
-        if (platform) {
-          this.targetPlatform = platform;
-          this.enemy.jump = true;
-          this.enemy.goRight = true; // перенести в обработку targetPlatform
-        }
+      this.enemy.jump = true;
+      if (this.enemy.yv > -this.enemy.jumpForce * 0.95 && !this.isJumping) return;
+      this.isJumping = true;
+
+      switch (true) {
+        case this.checkLeftPlat(this.targetPlatform):
+          if (this.enemy.x > (this.targetPlatform.x + this.targetPlatform.w + 30)) {
+            this.enemy.goLeft = true;
+          } else if ((this.enemy.x + this.enemy.w) <= (this.targetPlatform.x + this.targetPlatform.w)) {
+            this.enemy.goLeft = false;
+            this.enemy.jump = false;
+          } else if ((this.enemy.y + this.enemy.h) < this.targetPlatform.y) {
+            this.enemy.goLeft = true;
+          } else {
+            this.enemy.goLeft = false;
+          }
+          break;
+
+        case this.checkRightPlat(this.targetPlatform):
+          if ((this.enemy.x + this.enemy.w) < (this.targetPlatform.x - 30)) {
+            this.enemy.goRight = true;
+          } else if (this.enemy.x >= this.targetPlatform.x) {
+            this.enemy.goRight = false;
+            this.enemy.jump = false;
+          } else if ((this.enemy.y + this.enemy.h) < this.targetPlatform.y) {
+            this.enemy.goRight = true;
+          } else {
+            this.enemy.goRight = false;
+          }
+          break;
+
+        default:
+          this.targetPlatform = null;
+          break;
       }
 
     } else {
       this.enemy.goLeft = false;
       this.enemy.goRight = false;
       this.enemy.jump = false;
-    }
-    // } else if (this.enemy.x > (this.game.hero.x + 2 * this.game.hero.w)) {
-    //   this.enemy.goLeft = Math.random() < 0.8;
-    //   this.enemy.goRight = false;
-    //
-    // } else {
-    //   this.enemy.goLeft = false;
-    //   this.enemy.goRight = false;
-    // }
 
-    // if (this.enemy.y > this.game.hero.y) {
-    //   this.enemy.jump = true;
-    // } else {
-    //   this.enemy.jump = false;
-    // }
+      // if (!this.enemy.wasBottomCollision) return;
+
+      if ((this.enemy.x + this.enemy.w) < this.game.hero.x) {
+
+        if ((this.enemy.x + this.enemy.w) < (this.enemy.lastBottomCollidePlatform.x + this.enemy.lastBottomCollidePlatform.w)) {
+          this.enemy.goRight = true;
+        } else {
+          this.targetPlatform = this.getPlatform("right");
+        }
+
+      } else if (this.enemy.x > (this.game.hero.x + this.game.hero.w)) {
+
+        if (this.enemy.x > this.enemy.lastBottomCollidePlatform.x) {
+          this.enemy.goLeft = true;
+        } else {
+          this.targetPlatform = this.getPlatform("left");
+        }
+      }
+    }
   }
 
-  getRightPlat() {
-    let platform;
+  getPlatform(side) {
+    let platform = null;
+    let check;
+    switch (side) {
+      case "left":
+        check = this.checkLeftPlat.bind(this);
+        break;
+      case "right":
+        check = this.checkRightPlat.bind(this);
+        break;
+      default:
+        throw new Error(`Неправильная сторона. side=${side}`);
+    }
 
     this.game.platforms.forEach(p => {
-      const distance = p.x - (this.enemy.x + this.enemy.w);
-      const height = p.y - (this.enemy.y + this.enemy.h);
-      if (distance > 0 && distance < 300 &&
-          height < this.maxJumpHeight) {
-        platform = platform ? (p.y < platform.y) ? p : platform : p;
+      if (check(p)) {
+        platform = platform ? ((p.y < platform.y) ? p : platform) : p;
       }
     });
 
     return platform;
   }
 
+  checkRightPlat(p) {
+    const distance = p.x - (this.enemy.lastBottomCollidePlatform.x + this.enemy.lastBottomCollidePlatform.w);
+    const height = -p.y + this.enemy.lastBottomCollidePlatform.y;
 
-  // update() {
-  //   if ((this.enemy.x + this.enemy.w) < (this.game.hero.x - this.game.hero.w)) {
-  //     this.enemy.goLeft = false;
-  //     // this.enemy.goRight = true;
-  //     this.enemy.goRight = Math.random() < 0.8;
-  //   } else if (this.enemy.x > (this.game.hero.x + 2 * this.game.hero.w)) {
-  //     // this.enemy.goLeft = true;
-  //     this.enemy.goLeft = Math.random() < 0.8;
-  //     this.enemy.goRight = false;
-  //   } else {
-  //     this.enemy.goLeft = Math.random() < 0.3;
-  //     this.enemy.goRight = Math.random() < 0.3;
-  //   }
-  //
-  //   if (this.enemy.y > this.game.hero.y) {
-  //     this.enemy.jump = true;
-  //   } else {
-  //     this.enemy.jump = false;
-  //   }
-  // }
+    return (distance > 0 &&
+        distance < 300 &&
+        height < this.maxJumpHeight &&
+        p.y > 0)
+  }
+
+  checkLeftPlat(p) {
+    const distance = this.enemy.lastBottomCollidePlatform.x - (p.x + p.w);
+    const height = -p.y + this.enemy.lastBottomCollidePlatform.y;
+
+    return (distance > 0 &&
+        distance < 300 &&
+        height < this.maxJumpHeight &&
+        p.y > 0)
+  }
 }
 
 export default EnemyAI
