8948a21babe05a974655409b858e8973df4ff2e4
Alex Kashko <alexkrond@yahoo.com>
2019-08-28 13:52:11 +0300

    ISSUE-57 Оружию добавлено вращение в сторону стрельбы

--- v3.0/js/СrashedPlatform.js
+++ v3.0/js/СrashedPlatform.js
@@ -9,23 +9,18 @@ class CrashedPlatform extends GameObject {
 
   draw(ctx) {
     const angle = 35;
-
     ctx.fillStyle = this.color;
 
     ctx.save();
     ctx.translate(this.x, this.y);
-
     ctx.rotate(angle * Math.PI / 180);
-
     ctx.fillRect(0, 0, this.w / 2, this.h);
-
     ctx.restore();
+
     ctx.save();
     ctx.translate(this.x + this.w, this.y);
-
     ctx.rotate((90 - angle) * Math.PI / 180);
     ctx.fillRect(0, 0, this.h, this.w / 2);
-
     ctx.restore();
   }
 }
